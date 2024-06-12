51f3be5c2f073157945e8a18322260bcccba663e
andresombra <andresombra.net@gmail.com>
2024-06-12 18:17:49 -0300

    Inxluido Login e Postage

---
+++ BlogSimples/BlogSimples.Web/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BlogSimples.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogSimples.Web.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Login> Logins { get; set; }
+        public DbSet<Postagens> Postagen { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("AppDb");
+        }
+    }
+}
---
+++ BlogSimples/BlogSimples.Web/Models/Postagem.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogSimples.Web.Models
+{
+    public class Postagem
+    {
+        public int PostId { get; set; }
+        public string Titulo { get; set; }
+        public string Postage { get; set; }
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+    }
+}
