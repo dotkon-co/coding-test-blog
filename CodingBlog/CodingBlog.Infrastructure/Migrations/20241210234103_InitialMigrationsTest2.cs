aa0f75e8abbe90d06cbb2504b82d8f1cd84b9338
chronosinfo <chronosinfo@chronosinfo.com.br>
2024-12-17 09:42:36 -0300

    add migracao

---
+++ CodingBlog/CodingBlog.Infrastructure/Migrations/20241210234103_InitialMigrationsTest2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CodingBlog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigrationsTest2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    Password = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    Role = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Password", "Role", "Username" },
+                values: new object[,]
+                {
+                    { 1, "admin@email.com", "$2a$11$Bkac.XCkFgUO2QLRkZDxHOgRw4nZhgODFWQBRkwLKBZdcwpeqvAwu", "Admin", "admin" },
+                    { 2, "user@email.com", "$2a$11$an8.T6LF7cAc8/1azzyQXeG5Z0YjpU/lNuOYqzHi9ldYmKXIgTAfy", "User", "user" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "CreatedAt", "Title", "UserId" },
+                values: new object[,]
+                {
+                    { 1, "Content of Post 1", new DateTime(2024, 12, 10, 20, 41, 3, 357, DateTimeKind.Local).AddTicks(2068), "Post 1", 1 },
+                    { 2, "Content of Post 2", new DateTime(2024, 12, 10, 20, 41, 3, 357, DateTimeKind.Local).AddTicks(2198), "Post 2", 2 },
+                    { 3, "Content of Post 3", new DateTime(2024, 12, 10, 20, 41, 3, 357, DateTimeKind.Local).AddTicks(2200), "Post 3", 1 },
+                    { 4, "Content of Post 4", new DateTime(2024, 12, 10, 20, 41, 3, 357, DateTimeKind.Local).AddTicks(2202), "Post 4", 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
